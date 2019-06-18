8ff6a8410ebd715483d94744053ca60832fcc79f
fhagno@yahoo.com <fhagno@yahoo.com>
2019-06-18 14:38:53 +0200

    Updates

---
+++ precache-manifest.1cf3c3ee9fddecbd135ca86902e352fe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e59f4896adb6eaadbf7",
+    "url": "/fhagno-portfolio-app/static/css/main.d75b1837.chunk.css"
+  },
+  {
+    "revision": "7e59f4896adb6eaadbf7",
+    "url": "/fhagno-portfolio-app/static/js/main.7e59f489.chunk.js"
+  },
+  {
+    "revision": "8fe3ab9d67359e261052",
+    "url": "/fhagno-portfolio-app/static/js/1.8fe3ab9d.chunk.js"
+  },
+  {
+    "revision": "3deab2e870566ccd9a1d",
+    "url": "/fhagno-portfolio-app/static/js/runtime~main.3deab2e8.js"
+  },
+  {
+    "revision": "61563c91fe872f223c634fab71733c01",
+    "url": "/fhagno-portfolio-app/static/media/3littleones2.61563c91.jpg"
+  },
+  {
+    "revision": "ece95fe3d60d23c23f6ff39c8d3dc143",
+    "url": "/fhagno-portfolio-app/static/media/3littlePrincesses.ece95fe3.jpg"
+  },
+  {
+    "revision": "1a947471d1fc6d6e58e5245ad9ea37ba",
+    "url": "/fhagno-portfolio-app/static/media/78.1a947471.jpg"
+  },
+  {
+    "revision": "bb3028181857cffdce5e63ebd0a67219",
+    "url": "/fhagno-portfolio-app/index.html"
+  }
+];
